0181a62645a2e28bfe53fe032b514be752a657cc
mustakrakishe <mustakrkish@gmail.com>
2022-05-09 00:00:39 +0300

    Create Character model && migration

---
+++ app/Models/Character.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Character extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'name',
+        'birthday',
+        'occupations',
+        'img',
+        'nickname',
+        'portrayed',
+    ];
+}
