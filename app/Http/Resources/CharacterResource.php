9c545c093dc4de30458ad1f23c604ffc00197ed4
mustakrakishe <mustakrkish@gmail.com>
2022-05-10 01:11:46 +0300

    Create CharacterResource

---
+++ app/Http/Resources/CharacterResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CharacterResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'birthday' => $this->birthday,
+            'occupations' => $this->occupations,
+            'img' => $this->img,
+            'nickname' => $this->nickname,
+            'portrayed' => $this->portrayed,
+            'episode_ids' => $this->episodes->pluck('id'),
+            'qoute_ids' => $this->quotes->pluck('id'),
+        ];
+    }
+}
