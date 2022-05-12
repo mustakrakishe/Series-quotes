802965b2348f2695d579b6d4c1d1369ac5fac4d0
mustakrakishe <mustakrkish@gmail.com>
2022-05-13 02:47:37 +0300

    Create QuoteResource && QuoteCollection

---
+++ app/Http/Resources/QuoteResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuoteResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'quote' => $this->quote,
+            'episode' => new EpisodeResource($this->episode),
+            'character' => new CharacterResource($this->character),
+        ];
+    }
+}
