43f3c9e38785300ec2e1a754dba113a766609045
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-18 01:57:37 +0300

    Remove unused classes from Exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,6 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Illuminate\Http\JsonResponse;
-use JsonException;
 use Throwable;
 
 class Handler extends ExceptionHandler
