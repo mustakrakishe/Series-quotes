bcb6a094c6b1f0f6ad0db70f16da10d6cf99b234
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-20 00:16:46 +0300

    Update Exception handler
    Remove annotations.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,46 +8,16 @@ use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
     protected $dontFlash = [
         'password',
         'password_confirmation',
     ];
-
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
 
     protected function shouldReturnJson($request, Throwable $e)
     {
         return true;
     }
 
-    /**
-     * Convert the given exception to an array.
-     *
-     * @param  \Throwable  $e
-     * @return array
-     */
     protected function convertExceptionToArray(Throwable $e)
     {
         $array = parent::convertExceptionToArray($e);
@@ -55,13 +25,6 @@ class Handler extends ExceptionHandler
         return $array;
     }
 
-    /**
-     * Convert a validation exception into a JSON response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Validation\ValidationException  $exception
-     * @return \Illuminate\Http\JsonResponse
-     */
     protected function invalidJson($request, ValidationException $exception)
     {
         return response()->json([
