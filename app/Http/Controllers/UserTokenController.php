2408c8451796144bb5495c0111a4b9bd5c38322e
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-21 20:32:24 +0300

    Create UserTokenController->index() and ->destroy()

---
+++ app/Http/Controllers/UserTokenController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserTokenController extends Controller
+{
+    public function index(User $user)
+    {
+        return view('users.tokens.index', compact('user'))->render();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @return \Illuminate\Http\Response
+     * @param  \App\Models\User  $user
+     */
+    public function destroy(Request $request, User $user)
+    {
+        $success = $user->tokens()->where('id', $request->token)->delete();
+
+        $user->refresh();
+
+        return view('users.tokens.index', compact('user', 'success'));
+    }
+}
