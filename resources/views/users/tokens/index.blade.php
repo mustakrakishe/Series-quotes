1c7930c18ce3e6f1d31dca637e98e46897f539ac
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-22 10:11:38 +0300

    Create users.tokens.index page view

--- resources/views/users/tokens/index.blade.php
+++ resources/views/users/tokens/index.blade.php
@@ -1,21 +1,25 @@
 <x-layout :title="'Tokens'">
 
-    <table>
-        <th>
-            <td>Name</td>
-            <td>Actions</td>
-        </th>
+    <table class="table">
+        <thead>
+            <th>
+                <td>Name</td>
+                <td>Actions</td>
+            </th>
+        </thead>
 
-        @foreach($user->tokens as $token)
-        <tr>
-            <td>{{ $token->name }}</td>
-            <td>
-                <form action="{{ route('users.tokens.destroy', ['user' => $user, 'token' => $token]) }}" method="post">
-                    @method('delete')
-                    @csrf
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-        </tr>
-        @endforeach
+        <tbody>
+            @foreach($user->tokens as $token)
+            <tr>
+                <td>{{ $token->name }}</td>
+                <td>
+                    <form action="{{ route('users.tokens.destroy', ['user' => $user, 'token' => $token]) }}" method="post">
+                        @method('delete')
+                        @csrf
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+            </tr>
+            @endforeach
+        </tbody>
     </table>
 </x-layout>
