cbd9963f00f86a125b844ffb34b86f631a938b1d
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-22 17:01:10 +0300

    Create UserTokenRepositoury->create()

--- app/Repositories/UserTokenRepository.php
+++ app/Repositories/UserTokenRepository.php
@@ -6,5 +6,10 @@ use App\Models\User;
 
 class UserTokenRepository
 {
-    // 
+    public function create(User $user, string $tokenName)
+    {
+        $token = $user->createToken($tokenName);
+        
+        return $token->plainTextToken;
+    }
 }
