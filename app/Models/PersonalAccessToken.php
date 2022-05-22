e6df4bbab7484ea9d6c15d76c67be599e6a0026c
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-22 22:47:06 +0300

    Set Token default orderBy to created_at desc

--- app/Models/PersonalAccessToken.php
+++ app/Models/PersonalAccessToken.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Laravel\Sanctum\PersonalAccessToken as SanctumPersonalAccessToken;
 
 class PersonalAccessToken extends SanctumPersonalAccessToken
 {
-    // 
+    protected static function booted()
+    {
+        static::addGlobalScope('orderByScope', function (Builder $builder) {
+            $builder->orderByDesc('created_at');
+        });
+    }
 }
