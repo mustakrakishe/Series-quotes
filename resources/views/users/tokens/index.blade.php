e02cac430c8688504dd6090dfc055bd3bda00914
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-22 19:58:49 +0300

    Update users.tokens.index page view
    Add created token info. Add Tokens table created_at column.

--- resources/views/users/tokens/index.blade.php
+++ resources/views/users/tokens/index.blade.php
@@ -1,31 +1,47 @@
 <x-layout :title="'Tokens'">
+
+    @isset($token)
+    <div class="alert alert-warning" role="alert">
+        <div>Here is your new Bearer Token.</div>
+        <div>Store it, cause you will never see it again!</div>
+
+        <x-input value="{{ $token->plainTextToken }}" name="new_token" readonly>{{ $token->accessToken->name }}</x-input>
+    </div>
+    @endisset
+
     <h1 class="h3 mb-3 fw-normal">Here are your tokens:</h1>
 
     <form id="create-token-form" action="{{ route('users.tokens.store', ['user' => $user]) }}" method="post">
         @csrf
     </form>
 
-    <table class="table mt-3">
+    <table id="tokens-table" class="table mt-3 mx-auto">
         <thead>
             <th>Name</th>
+            <th>Created at</th>
             <th>Actions</th>
         </thead>
 
         <tbody>
             <tr>
-                <td class="align-middle"><x-input form="create-token-form" type="text" name="name" placeholder="Name">Name</x-input></td>
-                <td class="align-middle"><button form="create-token-form" type="submit" class="btn btn-primary">Create new</button></td>
+                <td class="align-middle">
+                    <x-input form="create-token-form" type="text" id="create-token-name-input" name="name" placeholder="Name" value="auth">New Token Name</x-input>
+                </td>
+                <td></td>
+                <td class="align-middle"><button form="create-token-form" type="submit" class="btn btn-primary">Create</button></td>
             </tr>
 
             @foreach($user->tokens as $token)
             <tr>
-                <td class="align-middle">{{ $token->name }}</td>
-                <td class="align-middle">
+                <td>{{ $token->name }}</td>
+                <td>{{ $token->created_at }}</td>
+                <td>
                     <form action="{{ route('users.tokens.destroy', ['user' => $user, 'token' => $token]) }}" method="post">
                         @method('delete')
                         @csrf
                         <button type="submit" class="btn btn-danger">Delete</button>
                     </form>
+                </td>
             </tr>
             @endforeach
         </tbody>
