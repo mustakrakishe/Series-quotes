abf39844ba2a243dbcbfeda1208cb5678ba1ade3
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-22 13:33:26 +0300

    Update users.tokens.indes page
    Add create token form. Fix table cell vertical aligment to middle.

--- resources/views/users/tokens/index.blade.php
+++ resources/views/users/tokens/index.blade.php
@@ -1,18 +1,26 @@
 <x-layout :title="'Tokens'">
+    <h1 class="h3 mb-3 fw-normal">Here are your tokens:</h1>
 
-    <table class="table">
+    <form id="create-token-form" action="{{ route('users.tokens.store', ['user' => $user]) }}" method="post">
+        @csrf
+    </form>
+
+    <table class="table mt-3">
         <thead>
-            <th>
-                <td>Name</td>
-                <td>Actions</td>
-            </th>
+            <th>Name</th>
+            <th>Actions</th>
         </thead>
 
         <tbody>
+            <tr>
+                <td class="align-middle"><x-input form="create-token-form" type="text" name="name" placeholder="Name">Name</x-input></td>
+                <td class="align-middle"><button form="create-token-form" type="submit" class="btn btn-primary">Create new</button></td>
+            </tr>
+
             @foreach($user->tokens as $token)
             <tr>
-                <td>{{ $token->name }}</td>
-                <td>
+                <td class="align-middle">{{ $token->name }}</td>
+                <td class="align-middle">
                     <form action="{{ route('users.tokens.destroy', ['user' => $user, 'token' => $token]) }}" method="post">
                         @method('delete')
                         @csrf
@@ -22,4 +30,5 @@
             @endforeach
         </tbody>
     </table>
+
 </x-layout>
