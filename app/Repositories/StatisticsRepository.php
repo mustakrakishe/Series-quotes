2dc11e7568b1ccb643664dc9fbcc1e85dc81990c
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-28 15:16:57 +0300

    Create StatisticsRepository->updateTotal()

--- app/Repositories/StatisticsRepository.php
+++ app/Repositories/StatisticsRepository.php
@@ -16,6 +16,11 @@ class StatisticsRepository
         return Redis::get('api-total-requests');
     }
 
+    public function updateTotal()
+    {
+        Redis::set('api-total-request', $this->countTotalRequests());
+    }
+
     public function countTotalRequests() {
         return array_reduce($this->getKeysStartWith('api:users:'), function ($sum, $cacheCellKeyName) {
             return $sum + Redis::get($cacheCellKeyName);
