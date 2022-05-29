02e178fe876ce5c6354e58265800943201163f3d
Andriy Baranik <abaranik@5prosoftware.com>
2022-05-30 00:58:01 +0300

    Clean webhook route file

--- routes/webhook.php
+++ routes/webhook.php
@@ -15,26 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('telegram', function () {
-    $telegram = new \Telegram\Bot\Api(env('TELEGRAM_BOT_TOKEN'));
-    $telegram->addCommands([
-        \Telegram\Bot\Commands\HelpCommand::class,
-        \App\Services\Telegram\Commands\StartCommand::class,
-    ]);
-    $telegram->commandsHandler(true);
-
-    // $update = Telegram::commandsHandler(true);
-    // return 'ok';
-});
-
-Route::post('telegram/send', function () {
-    return Telegram::sendMessage([
-        'chat_id' => '810370065', 
-        'text' => 'Hello World'
-      ]);
-});
-
-Route::post('telegram/get-updates', function () {
-    return Telegram::getUpdates();
-    // $telegram = new \Telegram\Bot\Api(env('TELEGRAM_BOT_TOKEN'));
-    // return $telegram->getUpdates();
+    Telegram::commandsHandler(true);
 });
